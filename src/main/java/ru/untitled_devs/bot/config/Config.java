de9e7c58eee7bbe5abdcc1f1b29a699733800e24
Alexander Bazarov <sanya_fritz@vk.com>
2025-06-27 22:55:13 +0500

    Изменен модификатор доступа для конфигурационных объектов на final

--- src/main/java/ru/untitled_devs/bot/config/Config.java
+++ src/main/java/ru/untitled_devs/bot/config/Config.java
@@ -1,9 +1,9 @@
 package ru.untitled_devs.bot.config;
 
 public final class Config {
-	private static BotConfig botConfig = new BotConfig();
-	private static MongoConfig mongoConfig = new MongoConfig();
-	private static GeocodingConfig geocodingConfig = new GeocodingConfig();
+	private static final BotConfig botConfig = new BotConfig();
+	private static final MongoConfig mongoConfig = new MongoConfig();
+	private static final GeocodingConfig geocodingConfig = new GeocodingConfig();
 
 	public static BotConfig getBotConfig() {
 		return botConfig;
