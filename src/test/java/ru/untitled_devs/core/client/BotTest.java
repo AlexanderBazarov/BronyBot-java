a3ef877f50ba976b8972b2c6284a9109812e7178
Alexander Bazarov <sanya_fritz@vk.com>
2025-06-28 18:19:37 +0500

    Удалены неиспользуемые импорты в классе BotTest

--- src/test/java/ru/untitled_devs/core/client/BotTest.java
+++ src/test/java/ru/untitled_devs/core/client/BotTest.java
@@ -1,7 +1,6 @@
 package ru.untitled_devs.core.client;
 
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
