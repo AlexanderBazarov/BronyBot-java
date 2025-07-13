4b9d9d5edace63309a18d7c210c925d7a2d89a5d
Alexander Bazarov <sanya_fritz@vk.com>
2025-07-14 03:07:34 +0500

    Обновлен интерфейс Middleware: заменен параметр метода preHandle на UpdateContext для унификации обработки обновлений.

--- src/main/java/ru/untitled_devs/core/middlewares/Middleware.java
+++ src/main/java/ru/untitled_devs/core/middlewares/Middleware.java
@@ -1,8 +1,9 @@
 package ru.untitled_devs.core.middlewares;
 
 import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.untitled_devs.core.context.UpdateContext;
 import ru.untitled_devs.core.fsm.context.FSMContext;
 
 public interface Middleware {
-    boolean preHandle(Update update, FSMContext ctx);
+    boolean preHandle(UpdateContext update, FSMContext ctx);
 }
