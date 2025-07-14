a55058ede00f4fb889ca0398d419147135477971
Alexander Bazarov <sanya_fritz@vk.com>
2025-07-15 03:27:12 +0500

    Обновлен класс StartRouter: изменено наследование с Router на UpdateRouter для улучшения обработки обновлений.

--- src/main/java/ru/untitled_devs/bot/features/start/StartRouter.java
+++ src/main/java/ru/untitled_devs/bot/features/start/StartRouter.java
@@ -4,12 +4,12 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import ru.untitled_devs.core.client.PollingClient;
 import ru.untitled_devs.core.fsm.context.FSMContext;
 import ru.untitled_devs.core.fsm.states.DefaultStates;
-import ru.untitled_devs.core.routers.Router;
+import ru.untitled_devs.core.routers.UpdateRouter;
 import ru.untitled_devs.core.routers.filters.CommandStart;
 import ru.untitled_devs.core.routers.handlers.MessageHandler;
 
 
-public final class StartRouter extends Router {
+public final class StartRouter extends UpdateRouter {
     private final PollingClient bot;
     public StartRouter(PollingClient bot) {
         this.bot = bot;
