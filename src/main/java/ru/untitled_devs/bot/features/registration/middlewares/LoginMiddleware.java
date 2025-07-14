9f149c665a8d8cb5a541a0136814d556a1aa1387
Alexander Bazarov <sanya_fritz@vk.com>
2025-07-15 03:28:50 +0500

    Обновлен класс LoginMiddleware для использования SceneManager и изменения в методе preHandle для обработки входа в сцену регистрации.

--- src/main/java/ru/untitled_devs/bot/features/registration/middlewares/LoginMiddleware.java
+++ src/main/java/ru/untitled_devs/bot/features/registration/middlewares/LoginMiddleware.java
@@ -1,18 +1,26 @@
 package ru.untitled_devs.bot.features.registration.middlewares;
 
-import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.untitled_devs.bot.features.registration.RegistrationStates;
+import ru.untitled_devs.core.context.UpdateContext;
 import ru.untitled_devs.core.fsm.context.FSMContext;
 import ru.untitled_devs.core.fsm.states.State;
 import ru.untitled_devs.core.fsm.states.StatesGroup;
 import ru.untitled_devs.core.middlewares.Middleware;
+import ru.untitled_devs.core.routers.scenes.SceneManager;
 
 public class LoginMiddleware implements Middleware {
+	private final SceneManager sceneManager;
+
+	public LoginMiddleware(SceneManager sceneManager) {
+		this.sceneManager = sceneManager;
+	}
+
 	@Override
-	public boolean preHandle(Update update, FSMContext ctx) {
+	public boolean preHandle(UpdateContext update, FSMContext ctx) {
 		State state = ctx.getState();
 		if (! StatesGroup.contains(RegistrationStates.class, state)) {
-			ctx.setState(RegistrationStates.START);
+			sceneManager.enterScene("register", update.getChatId(), ctx);
+			return false;
 		}
 		return true;
 	}
