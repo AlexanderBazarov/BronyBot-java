d3b7b14dac883d175a6ec7a524539b2dc9665873
Alexander Bazarov <sanya_fritz@vk.com>
2025-07-15 03:28:04 +0500

    Обновлен класс Main: добавлен менеджер сцен для обработки регистрации и локализации, изменены маршрутизаторы и добавлены новые зависимости.

--- src/main/java/ru/untitled_devs/bot/Main.java
+++ src/main/java/ru/untitled_devs/bot/Main.java
@@ -9,24 +9,25 @@ import org.apache.logging.log4j.Logger;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import ru.untitled_devs.bot.config.Config;
-import ru.untitled_devs.bot.features.localisation.LocalisationRouter;
+import ru.untitled_devs.bot.features.localisation.LocalisationScene;
 import ru.untitled_devs.bot.features.localisation.midlewares.LocalisationMiddleware;
-import ru.untitled_devs.bot.features.registration.RegistrationRouter;
+import ru.untitled_devs.bot.features.registration.RegistrationScene;
 import ru.untitled_devs.bot.features.registration.middlewares.LoginMiddleware;
 import ru.untitled_devs.bot.features.start.StartRouter;
 import ru.untitled_devs.bot.shared.geocoder.Geocoder;
 import ru.untitled_devs.bot.shared.geocoder.yandex.YandexGeocoder;
+import ru.untitled_devs.bot.shared.image.ImageService;
 import ru.untitled_devs.core.client.PollingClient;
 import ru.untitled_devs.core.dispatcher.Dispatcher;
 import ru.untitled_devs.core.fsm.storage.InMemoryStorage;
+import ru.untitled_devs.core.routers.scenes.SceneManager;
 
 public class Main {
-
-
     public static void main(String[] args) {
 		InMemoryStorage storage = new InMemoryStorage();
 		Logger logger = LogManager.getLogger();
-		Dispatcher dispatcher = new Dispatcher(storage, logger);
+		SceneManager sceneManager = new SceneManager();
+		Dispatcher dispatcher = new Dispatcher(storage, logger, sceneManager);
 
 		MongoClient client = MongoClients.create(Config.getMongoConfig().getMongoString());
 		Datastore datastore = Morphia.createDatastore(client, Config.getMongoConfig().getMongoDBName());
@@ -34,19 +35,21 @@ public class Main {
 		Geocoder geocoder =
 			new YandexGeocoder(Config.getGeocodingConfig().getApiUrl(),
 				Config.getGeocodingConfig().getApiKey());
-
+		ImageService imageService = new ImageService(Config.getImagesConfig().getImagesPath(), datastore);
 
 		try {
 			TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             PollingClient bot = new PollingClient(Config.getBotConfig().getBotToken(),
 				Config.getBotConfig().getBotName(), dispatcher, logger);
 
-			dispatcher.addMiddleware(new LocalisationMiddleware());
-			dispatcher.addMiddleware(new LoginMiddleware());
+			sceneManager.register(new RegistrationScene(bot, datastore, geocoder, imageService));
+			sceneManager.register(new LocalisationScene(bot));
+
+			dispatcher.addMiddleware(new LocalisationMiddleware(sceneManager));
+			dispatcher.addMiddleware(new LoginMiddleware(sceneManager));
 
 			dispatcher.addRouter(new StartRouter(bot));
-			dispatcher.addRouter(new RegistrationRouter(bot, datastore, geocoder));
-			dispatcher.addRouter(new LocalisationRouter(bot));
+			dispatcher.addRouter(new LocalisationScene(bot));
 
             botsApi.registerBot(bot);
         } catch (Exception e) {
