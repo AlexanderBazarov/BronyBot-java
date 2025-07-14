9d48e1a6c6c1480621061ec418ba3e46344222da
Alexander Bazarov <sanya_fritz@vk.com>
2025-07-15 03:23:24 +0500

    Добавлены методы equals и hashCode в класс Coordinates для корректного сравнения объектов.

--- src/main/java/ru/untitled_devs/bot/shared/geocoder/Coordinates.java
+++ src/main/java/ru/untitled_devs/bot/shared/geocoder/Coordinates.java
@@ -1,5 +1,7 @@
 package ru.untitled_devs.bot.shared.geocoder;
 
+import java.util.Objects;
+
 public class Coordinates {
 	private final double latitude;
 	private final double longitude;
@@ -20,5 +22,17 @@ public class Coordinates {
 	@Override
 	public String toString() {
 		return latitude + "," + longitude;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass()) return false;
+		Coordinates that = (Coordinates) o;
+		return Double.compare(latitude, that.latitude) == 0 && Double.compare(longitude, that.longitude) == 0;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(latitude, longitude);
 	}
 }
