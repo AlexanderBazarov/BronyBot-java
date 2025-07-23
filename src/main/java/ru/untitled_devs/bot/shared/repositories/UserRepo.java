9d9a712a0fec48ceca4f02a93cc6acd8af371c6e
Alexander Bazarov <sanya_fritz@vk.com>
2025-07-23 22:58:48 +0700

    Добавлен метод для получения пользователя по Telegram ID в классе UserRepo

--- src/main/java/ru/untitled_devs/bot/shared/repositories/UserRepo.java
+++ src/main/java/ru/untitled_devs/bot/shared/repositories/UserRepo.java
@@ -3,8 +3,22 @@ package ru.untitled_devs.bot.shared.repositories;
 import dev.morphia.Datastore;
 import ru.untitled_devs.bot.shared.models.User;
 
+import java.util.Optional;
+
+import static dev.morphia.query.filters.Filters.eq;
+
 public final class UserRepo extends MongoRepo<User>{
+	private final Datastore datastore;
+
 	public UserRepo(Datastore datastore) {
 		super(datastore, User.class);
+
+		this.datastore = datastore;
 	}
+
+	public Optional<User> getByTelegramId(long telegramId) {
+		return Optional.ofNullable(datastore.find(User.class).filter(eq("telegramId", telegramId)).first());
+	}
+
+
 }
